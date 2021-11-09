c468a3c9fcf58dd8d1012cb0b02d34f706326c1a
mubasher-javed <mubashar.tech@gmail.com>
2021-11-09 18:23:56 +0500

    user div on sidebar changed

--- ngChat/src/app/components/message-form/message-form.component.ts
+++ ngChat/src/app/components/message-form/message-form.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-message-form',
   templateUrl: './message-form.component.html',
-  styleUrls: ['./message-form.component.css']
+  styleUrls: ['./message-form.component.css'],
 })
 export class MessageFormComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
